0f20bc8b8433881c07cb650f4655996b29a4ad19
kimpors <mikita_korshak@ukr.net>
2024-05-01 14:14:09 +0300

    Add debugger support(again)
    debugger support only c files

--- nvim/lua/plugins/code.lua
+++ nvim/lua/plugins/code.lua
@@ -231,6 +231,36 @@ return {
     end,
   },
 
+  -- debugger
+  {
+		"mfussenegger/nvim-dap",
+
+		lazy = true,
+		config = function()
+			local dap = require("dap")
+
+			dap.adapters.gdb = {
+				type = "executable",
+				command = "gdb",
+				args = { "-i", "dap" }
+			}
+
+
+			dap.configurations.c = {
+			  	{
+					name = "Launch",
+					type = "gdb",
+					request = "launch",
+					program = function()
+					  return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+					end,
+					cwd = "${workspaceFolder}",
+					stopAtBeginningOfMainSubprogram = false,
+				},
+			}
+		end,
+	},
+
       -- file browser
     {
         'stevearc/oil.nvim',
